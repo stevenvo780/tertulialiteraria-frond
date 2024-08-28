9f4964b4fb1004a00c335eff7dd25c0dbcb972e7
Stev <stevenvallejo780@gmail.com>
2024-08-28 12:18:19 -0500

    Actualizar dependencias de react-slick y slick-carousel

--- src/components/ScrollableEvents.tsx
+++ src/components/ScrollableEvents.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Card, Col, Row } from 'react-bootstrap';
+import Slider from 'react-slick';
+import { Card, Col } from 'react-bootstrap';
 import { FaBook, FaFeatherAlt, FaGlasses, FaNewspaper, FaPenFancy, FaScroll, FaUniversity } from 'react-icons/fa';
-import PerfectScrollbar from 'react-perfect-scrollbar';
-import 'react-perfect-scrollbar/dist/css/styles.css'; 
 import { Events } from '../utils/types';
 import { useNavigate } from 'react-router-dom';
 import { BsBookHalf, BsBook, BsJournalBookmark, BsPen } from 'react-icons/bs';
@@ -10,6 +9,8 @@ import { GiBookshelf, GiBookmark, GiQuillInk, GiOpenBook, GiScrollUnfurled, GiBr
 import { MdLibraryBooks, MdMenuBook, MdOutlineAutoStories, MdOutlineClass, MdOutlineSchool } from 'react-icons/md';
 import { IoBookSharp, IoSchoolSharp, IoJournal, IoNewspaperSharp } from 'react-icons/io5';
 import { RiArticleLine, RiBookLine, RiBookOpenLine, RiPencilLine } from 'react-icons/ri';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const iconList = [
   FaBook, FaFeatherAlt, FaPenFancy, FaScroll, FaGlasses, FaUniversity, FaNewspaper,
@@ -28,6 +29,37 @@ const ScrollableEvents: React.FC<ScrollableEventsProps> = ({ events }) => {
   const navigate = useNavigate();
   let iconIndex = 0;
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   const handleEventClick = (eventId: number | null) => {
     if (eventId !== null) {
       navigate(`/events/${eventId}`);
@@ -35,49 +67,39 @@ const ScrollableEvents: React.FC<ScrollableEventsProps> = ({ events }) => {
   };
 
   return (
-    <div className="d-flex align-items-center my-4 position-relative">
-      <PerfectScrollbar
-        style={{
-          display: 'flex',
-          overflowX: 'auto',
-          paddingLeft: '60px',
-          paddingRight: '60px',
-          width: '100%',
-        }}
-      >
-        <Row style={{ flexWrap: 'nowrap' }}>
-          {events.length > 0 ? (
-            events.map((event) => {
-              const IconComponent = iconList[iconIndex % iconList.length];
-              iconIndex++;
-              return (
-                <Col key={event.id} xs={4} md={3} style={{ padding: '0 10px' }}>
-                  <div
-                    style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', cursor: 'pointer' }}
-                    onClick={() => handleEventClick(event.id as number | null)}
+    <Slider {...settings}>
+      {events.length > 0 ? (
+        events.map((event) => {
+          const IconComponent = iconList[iconIndex % iconList.length];
+          iconIndex++;
+          return (
+            <div key={event.id}>
+              <Col style={{ padding: '0 10px' }}>
+                <div
+                  style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', cursor: 'pointer' }}
+                  onClick={() => handleEventClick(event.id as number | null)}
+                >
+                  <IconComponent size={82} style={{ marginBottom: '10px' }} />
+                  <Card
+                    className='card-events-home'
+                    style={{ minWidth: '100%', marginBottom: '10px' }}
                   >
-                    <IconComponent size={82} style={{ marginBottom: '10px' }} />
-                    <Card
-                      className='card-events-home'
-                      style={{ minWidth: '100%', marginBottom: '10px' }}
-                    >
-                      <Card.Body className="d-flex flex-column align-items-center" style={{ padding: 5 }}>
-                        <Card.Title>{event.title}</Card.Title>
-                        <Card.Text style={{ fontSize: 12 }}>
-                          {new Date(event.startDate).toLocaleDateString()} - {new Date(event.startDate).toLocaleTimeString()}
-                        </Card.Text>
-                      </Card.Body>
-                    </Card>
-                  </div>
-                </Col>
-              );
-            })
-          ) : (
-            <p>No hay eventos recurrentes</p>
-          )}
-        </Row>
-      </PerfectScrollbar>
-    </div>
+                    <Card.Body className="d-flex flex-column align-items-center" style={{ padding: 5 }}>
+                      <Card.Title>{event.title}</Card.Title>
+                      <Card.Text style={{ fontSize: 12 }}>
+                        {new Date(event.startDate).toLocaleDateString()} - {new Date(event.startDate).toLocaleTimeString()}
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </div>
+              </Col>
+            </div>
+          );
+        })
+      ) : (
+        <p>No hay eventos recurrentes</p>
+      )}
+    </Slider>
   );
 };
 
